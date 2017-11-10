88e609e98d81067cbe54c2fb4c58f442bb73cf6c
ntnguyen <ntnguyen@fossil.com>
2017-11-10 16:49:22 +0700

    Update token

--- bot.php
+++ bot.php
@@ -1,9 +1,13 @@
 <?php
 include 'FbBot.php';
 $tokken = $_REQUEST['hub_verify_token'];
-$hubVerifyToken = 'cloudwaysschool';
+
+$hubVerifyToken = 'nguyenkhoikhanh';
+
 $challange = $_REQUEST['hub_challenge'];
-$accessToken = 'nguyenkhoikhanh';
+
+$accessToken = 'EAACSABWtkLwBADpg1X9bzxbr3w2m8XmwFD97f6hIQeaZAZA6KKbNTwcW2mHW6BdZAEsorScK23E5MZAEU9CiJG8LVznKtjpEZAGjqP5JSkq8FrYkUY1dpSInbj3sOEZAEOvyfRvDa7atsM6WPeT4aAUAUGrusLbqqLy3YXTrWx2AZDZD';
+
 $bot = new FbBot();
 $bot->setHubVerifyToken($hubVerifyToken);
 $bot->setaccessToken($accessToken);
