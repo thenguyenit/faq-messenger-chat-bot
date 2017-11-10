988c5960c733f033bcf7459e15f24a70cf2d2852
ntnguyen <ntnguyen@fossil.com>
2017-11-10 17:37:30 +0700

    Add first message

--- bot.php
+++ bot.php
@@ -1,12 +1,12 @@
 <?php
 include 'FbBot.php';
-// var_dump($_REQUEST);die;
+
 $tokken = $_REQUEST['hub_verify_token'];
 $challange = $_REQUEST['hub_challenge'];
 
 $hubVerifyToken = 'nguyenkhoikhanh';
 
-$accessToken = 'EAACSABWtkLwBADpg1X9bzxbr3w2m8XmwFD97f6hIQeaZAZA6KKbNTwcW2mHW6BdZAEsorScK23E5MZAEU9CiJG8LVznKtjpEZAGjqP5JSkq8FrYkUY1dpSInbj3sOEZAEOvyfRvDa7atsM6WPeT4aAUAUGrusLbqqLy3YXTrWx2AZDZD';
+$accessToken = 'EAACSABWtkLwBAIBNWicbZBx8UtCUXmB7U64BjXxhUwrxmfXI3u0PWIMS6Xd74dAZCkCh5GelJZATrLQ938DyOF9aZCJgbQZBHRn1tmRtaFcNfTDmpZCRBNEgyvpfBtQ9oZB6HXL8CKDFrVQcv3soXBqjkYjbQPXEEXXRYB1HtZCmVwZDZD';
 
 $bot = new FbBot();
 
@@ -16,4 +16,13 @@ $bot->setaccessToken($accessToken);
 echo $bot->verifyTokken($tokken, $challange);
 $input = json_decode(file_get_contents('php://input'), true);
 $message = $bot->readMessage($input);
-$textmessage = $bot->sendMessage($message);
+if($message == 'hi') {                     
+	$answer = "Hello! How may I help you today ";                      
+	$response = [
+		'recipient' => [ 'id' => $senderId ],                         
+		'message' => [ 'text' => $answer ],                         
+		'access_token' => $this->accessToken                      
+	];
+}
+$textmessage = $bot->sendMessage($response);
+
