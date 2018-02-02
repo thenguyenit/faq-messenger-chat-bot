51be166bb4a3f6604376c50d077bacc50dc1603c
ntnguyen <ntnguyen@fossil.com>
2018-02-02 14:33:06 +0700

    Add icon for sync's suggestion

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,5 +2,5 @@
 
 define('LOG_PATH', ROOT_PATH . '/var/log');
 
-defined('STATIC_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/pub/static');
-defined('IMG_STATIC_URL', STATIC_URL . '/icon');
+define('STATIC_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/pub/static');
+define('IMG_STATIC_URL', STATIC_URL . '/icon');
